2f946c38f875caa30f61851b6e420ebe9d54f8c3
anwar-zakapps <127837751+anwar-zakapps@users.noreply.github.com>
2024-01-12 15:49:12 +0530

    Recurring Course API Implamented

--- src/app/shared/utils/url.utils.ts
+++ src/app/shared/utils/url.utils.ts
@@ -132,6 +132,6 @@ export class UrlUtils {
      POST_GET_SUMMARY_LIST: `${this.USER_URL}get-f2f-course-expiration-summary`,
      POST_EXPORT_MAIL_F2F: `${this.USER_URL}export-ff-list`,
      POST_GET_ACTIVE_COUNT: `${this.USER_URL}show-f2f-active-users-count`,
-
+     POST_GET_RECURRING_COURSE: `${this.USER_URL}get-ff-courses-recurring`,
   };
 }
